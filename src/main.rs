cfa8b64c3d5792d28accad38cf943c5c16ba6b60
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2024-08-26 17:33:29 -0400

    Switch to stack architecture (#2)
    * Switch to stack architecture
    
    * Remove superfluous prints
    
    * cfg on flags
    
    * fix accidental actions change
    
    * update Makefile
    
    ---------
    
    Co-authored-by: Yey007 <55263178+Yey007@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,3 @@
 // Copyright (C) 2024 Ethan Uppal. All rights reserved.
 
-use fernjit::executable_memory::ExecutableMemory;
-use std::{io, mem};
-
-fn main() -> io::Result<()> {
-    let mut jit_mem = ExecutableMemory::of_size(1024 * 1024)?;
-
-    for (i, byte) in [
-        0x48, 0xC7, 0xC0, 0x01, 0x00, 0x00, 0x02, 0x48, 0xC7, 0xC7, 0x2A, 0x00,
-        0x00, 0x00, 0x0F, 0x05
-    ]
-    .iter()
-    .enumerate()
-    {
-        jit_mem.as_slice_mut()[i] = *byte;
-    }
-
-    let func: fn() -> () = unsafe { mem::transmute(jit_mem.start()) };
-    func();
-
-    Ok(())
-}
+fn main() {}
